80e86911859a07be22449123fa06e77c33a3f1ce
Jorge <jorgediamanto01@gmail.com>
2022-12-25 23:44:14 +0100

    a

--- wha_bot.py
+++ wha_bot.py
@@ -1,3 +1,29 @@
 import pyautogui as pt
 import pyperclip as pc
-from pynput.mouse import Controller 
+from pynput.mouse import Controller , Button
+from time import sleep
+
+mouse = Controller()
+
+class Whatsapp:
+
+    def __init__(self,speed=.5,click_speed =.3):
+        self.speed = speed
+        self.click_speed = click_speed
+        self.message  = ""
+        self.last_message = ""
+
+    def nav_green_dot(self):
+        try:
+            position = pt.locateOnScreen("punto_verde.png",confidence=0.7)
+            pt.moveTo(position[0:2],duration=self.speed)
+            pt.moveRel(-100,0,duration=self.speed)
+            pt.doubleClick(interval=self.click_speed)
+        
+        except Exception as e:
+            print("Exception (nav_green_dot): ", e)
+
+wha_bot = Whatsapp(speed=.5,click_speed=.4)
+        
+sleep(3)        
+wha_bot.nav_green_dot()
